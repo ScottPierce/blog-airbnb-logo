cf77efc908be53e76b0a828b8eb7eaa4e13f7ee5
Scott Pierce <871169+ScottPierce@users.noreply.github.com>
2024-07-22 23:26:51 -0400

    save

--- airbnb-logo/src/main/java/dev/scottpierce/airbnb/logo/AirbnbLogo.kt
+++ airbnb-logo/src/main/java/dev/scottpierce/airbnb/logo/AirbnbLogo.kt
@@ -12,16 +12,19 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Stroke
-import dev.scottpierce.airbnb.logo.svg.svgCommandsToPath
 import kotlin.math.min
+import kotlin.math.pow
+import kotlin.math.sqrt
 
-private const val LOGO_VECTOR_WIDTH = 1991.3f
-private const val LOGO_VECTOR_HEIGHT = 2143.2f
-private const val LOGO_TOTAL_DISTANCE = 10837.511f // Calculated by the distance algorithm
-private const val STROKE_WIDTH = 140f
+private const val LOGO_VECTOR_WIDTH: Float = 1991.3f
+private const val LOGO_VECTOR_HEIGHT: Float = 2143.2f
+private const val LOGO_TOTAL_DISTANCE: Float = 23636.26f // Calculated by the distance algorithm
+private const val STROKE_WIDTH: Float = 140f
 
 @Composable
 fun AirbnbLogo(
@@ -31,7 +34,7 @@ fun AirbnbLogo(
 
     val drawPercent: Float by animateFloatAsState(
         targetValue = targetDrawPercent,
-        animationSpec = tween(2000),
+        animationSpec = tween(20000),
         label = "Draw Percent",
     )
 
@@ -62,6 +65,115 @@ fun DrawScope.drawSvg(color: Color, drawPercent: Float) {
         drawPercent = drawPercent,
     )
     drawPath(path, color, style = Stroke(width = scaledStrokeWidth))
+}
+
+internal fun svgCommandsToPath(
+    xScale: Float,
+    yScale: Float,
+    nodes: List<VectorNode>,
+    distanceToDraw: Float,
+    drawPercent: Float,
+): Path {
+    val path = Path()
+    var lastNode: VectorNode = nodes.first()
+    var distanceDrawn = 0f
+
+    // Start at the location right before the first command
+    path.moveTo(lastNode.p.x * xScale, lastNode.p.y * yScale)
+
+    for (i in 1 ..< (nodes.size + 1)) {
+        val node: VectorNode = if (i == nodes.size) {
+            // Finish the logo by ending with the first node
+            nodes[0]
+        } else {
+            nodes[i]
+        }
+
+        val commandDistance = calculateDistance(lastNode, node)
+        val commandDrawPercent: Float = if (commandDistance < (distanceToDraw - distanceDrawn)) {
+            1f
+        } else {
+            if (distanceDrawn >= distanceToDraw) {
+                // This can happen when the drawPercent is 0
+                break
+            }
+            ((distanceToDraw - distanceDrawn) / commandDistance).coerceAtMost(1f)
+        }
+
+        val c1: Point = lastNode.p1
+        val c2: Point = node.p2
+        val end: Point = node.p
+
+        if (commandDrawPercent == 1f) {
+            path.cubicTo(
+                c1.x * xScale, c1.y * yScale,
+                c2.x * xScale, c2.y * yScale,
+                end.x * xScale, end.y * yScale,
+            )
+        } else {
+            path.partialCubicBezier(
+                Offset(lastNode.p.x * xScale, lastNode.p.y * yScale),
+                Offset(c1.x * xScale, c2.y * yScale),
+                Offset(c2.x * xScale, c2.y * yScale),
+                Offset(end.x * xScale, end.y * yScale),
+                commandDrawPercent,
+            )
+        }
+
+        lastNode = node
+
+        distanceDrawn += commandDistance
+        if (distanceDrawn >= distanceToDraw) {
+            break
+        }
+    }
+
+    if (drawPercent >= 1f) {
+        // Close the loop to make sure we don't end up with any missing pieces
+        path.close()
+    }
+
+    println("Total path distance: $distanceDrawn")
+    return path
+}
+
+private fun calculateDistance(node1: VectorNode, node2: VectorNode): Float {
+    // We could calculate the distance of a cubic Bezier curve, and a quadratic Bezier curve, a
+    // simple magnitude calculation on the start and end points should be sufficient.
+    return sqrt((node2.p.x - node1.p.x).pow(2) + (node2.p.y - node1.p.x).pow(2))
+}
+
+@Suppress("LocalVariableName")
+private fun Path.partialCubicBezier(
+    start: Offset,
+    c1: Offset,
+    c2: Offset,
+    end: Offset,
+    drawPercent: Float
+) {
+    // Calculate intermediate points using De Casteljau's algorithm
+    val partialControl1 = linearInterpolation(start, c1, drawPercent)
+    val p1_1 = linearInterpolation(c1, c2, drawPercent)
+    val p2_1 = linearInterpolation(c2, end, drawPercent)
+
+    val partialControl2 = linearInterpolation(partialControl1, p1_1, drawPercent)
+    val p1_2 = linearInterpolation(p1_1, p2_1, drawPercent)
+
+    val partialEnd = linearInterpolation(partialControl2, p1_2, drawPercent)
+
+    // Draw the cubic Bézier curve segment
+    cubicTo(
+        partialControl1.x, partialControl1.y,
+        partialControl2.x, partialControl2.y,
+        partialEnd.x, partialEnd.y,
+    )
+}
+
+private fun linearInterpolation(a: Offset, b: Offset, t: Float): Offset {
+    return Offset(
+        x = (1 - t) * a.x + t * b.x,
+        y = (1 - t) * a.y + t * b.y
+    )
 }
 
 private val AIRBNB_LOGO_NODES: List<VectorNode> = listOf(
